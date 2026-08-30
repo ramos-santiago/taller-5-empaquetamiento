7a50a61acbde1b7e8ab9381c7baecd569b03d024
Santiago Ramos <s.ramose@uniandes.edu.co>
2026-08-30 18:40:57 -0500

    feat: prepare churn model package

---
+++ test/test-package.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+import pandas as pd
+from model.predict import make_prediction
+
+sample_input_data = pd.read_csv(Path(__file__).with_name("bankchurn_test.csv"))
+result = make_prediction(input_data=sample_input_data)
+print(result)
